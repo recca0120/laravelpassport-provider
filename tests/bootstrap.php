550a927dfa5ede3fbfa296854e33f8aeee7e3a32
recca0120 <recca0120@gmail.com>
2018-01-21 22:45:50 +0800

    fix tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -13,8 +13,6 @@
 
 require __DIR__.'/../vendor/autoload.php';
 
-use Carbon\Carbon;
-
 if (class_exists('PHPUnit\Framework\TestCase') === false) {
     class_alias('PHPUnit_Framework_TestCase', 'PHPUnit\Framework\TestCase');
 }
